b6d03f2d51c93110de10a640306ae4a0b97b8c6c
ludogernay <ludovic.gernay@ynov.com>
2024-03-26 12:45:29 +0100

    [+] subtitles fr and en

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function Product() {
   return (
-    <div className='h-3/4 w-1/2 border-red-600 border-2'>
+    <div className='h-3/4 w-1/2 pt-5'>
       <video controls className='h-full w-full'>
-        <source src='../../public/EldenRing.mp4' type='video/mp4'/>
-        <source src='../../public/EldenRing.webm' type='video/webm'/>
+        <source src='/EldenRing.mp4' type='video/mp4'/>
+        <source src='/EldenRing.webm' type='video/webm'/>
+        <track src='/EldenRing-en.vtt' kind='subtitles' srcLang='en' label='English'/>
+        <track src='/EldenRing-fr.vtt' kind='subtitles' srcLang='fr' label='Français'/>
         Your browser does not support the video tag.
       </video>
     </div>
