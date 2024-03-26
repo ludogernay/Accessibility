05b9cf47346b2b3c28e1a7d26e22200c881a95d5
ludogernay <ludovic.gernay@ynov.com>
2024-03-26 11:21:02 +0100

    [+] video

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Product() {
   return (
     <div className='h-3/4 w-1/2 border-red-600 border-2'>
-        <source src='' type='video/mp4'/>
+      <video controls className='h-full w-full'>
+        <source src='../../public/EldenRing.mp4' type='video/mp4'/>
+        <source src='../../public/EldenRing.webm' type='video/webm'/>
+        Your browser does not support the video tag.
+      </video>
     </div>
   )
 }
